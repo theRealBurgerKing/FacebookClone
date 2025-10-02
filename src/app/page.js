d7b054ab88c576e3c5077e4e1b9b140968da19ed
theRealBurgerKing <haotianwang2001@gmail.com>
2025-10-02 22:10:25 +1000

    Initial commit: Facebook Clone project

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Header from "@/components/Header";
+
+
+export default function Home() {
+  return (
+    <div>
+      {/*Header*/}
+      <Header />
+      {/*Feed*/}
+      {/*Widgets*/}
+      {/*Modal*/}
+    </div>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import Image from "next/image";
+import HeaderIcon from './HeaderIcon'
+import {
+  BellIcon,
+  ChatBubbleLeftIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  Squares2X2Icon,
+} from "@heroicons/react/24/solid";
+
+import {
+  FlagIcon,
+  PlayIcon,
+  MagnifyingGlassIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/24/outline";
+
+function Header() {
+  return (
+    <>
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
+      {/*Left*/}
+      <div className="flex items-center">
+        <Image src="https://links.papareact.com/5me" alt="logo" width={40} height={40} layout="fixed"/>
+        
+        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <MagnifyingGlassIcon className='h-6 w-6 text-gray-600'/>
+          <input type="text"
+          className="flex ml-2 items-center bg-transparent outline-none 
+          flex-shrink
+          placeholder:text-gray-600" 
+          placeholder='Search Facebook'/>
+        </div>
+      </div>
+
+
+      {/*Center*/}
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon active={true} Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon}/>
+          <HeaderIcon Icon={PlayIcon}/>
+          <HeaderIcon Icon={ShoppingCartIcon}/>
+          <HeaderIcon Icon={UserGroupIcon}/>
+        </div>
+      </div>
+      {/*Right*/}
+    </div>
+
+    </>)
+}
+
+export default Header
