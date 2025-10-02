3456b98cf8c7be766b0b8374db7b1fb48f5787a2
theRealBurgerKing <haotianwang2001@gmail.com>
2025-10-03 08:08:28 +1000

    feat: complete header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,11 @@ import Image from "next/image";
 import HeaderIcon from './HeaderIcon'
 import {
   BellIcon,
-  ChatBubbleLeftIcon,
+  ChatBubbleLeftIcon as ChatIcon,
   ChevronDownIcon,
   HomeIcon,
   UserGroupIcon,
-  Squares2X2Icon,
+  Squares2X2Icon as ViewGridIcon,
 } from "@heroicons/react/24/solid";
 
 import {
@@ -28,7 +28,7 @@ function Header() {
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <MagnifyingGlassIcon className='h-6 w-6 text-gray-600'/>
           <input type="text"
-          className="flex ml-2 items-center bg-transparent outline-none 
+          className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none 
           flex-shrink
           placeholder:text-gray-600" 
           placeholder='Search Facebook'/>
@@ -46,7 +46,20 @@ function Header() {
           <HeaderIcon Icon={UserGroupIcon}/>
         </div>
       </div>
+
+
+
       {/*Right*/}
+      <div className='flex items-center sm:space-x-2 justify-end'>
+        {/*Profile pic*/}
+        <p className="whitespace-nowrap font-semibold pr-3">
+          Amen
+        </p>
+        <ViewGridIcon className='icon' />
+        <ChatIcon className='icon' />
+        <BellIcon className='icon' />
+        <ChevronDownIcon className='icon' />
+      </div>
     </div>
 
     </>)
