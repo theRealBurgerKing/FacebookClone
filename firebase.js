6a864f65299920c805247ae73ce4bee16f35a4f0
theRealBurgerKing <haotianwang2001@gmail.com>
2025-10-04 17:04:43 +1000

    fix: use env_var

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDlDKTjV_TCAM7_I-VtkAjl2z4C_NVMPv4",
-    authDomain: "facebookclone-5e5d1.firebaseapp.com",
-    projectId: "facebookclone-5e5d1",
-    storageBucket: "facebookclone-5e5d1.firebasestorage.app",
-    messagingSenderId: "180485022891",
-    appId: "1:180485022891:web:f8da551e1f7749e2e503ee",
-    measurementId: "G-6QKKC0EC04"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // 初始化 Firebase
